78589e37f36a8b9e887e5fec70cd0e76d62e7e6a
Debleena18 <sarkardebleena18@gmail.com>
2022-04-29 11:55:39 +0530

    addind dummy data

--- src/pages/AllQuotes.js
+++ src/pages/AllQuotes.js
@@ -1,11 +1,12 @@
-import QuoteForm from '../components/quotes/QuoteForm';
+import QuoteList from '../components/quotes/QuoteList';
 
-const NewQuote = () => {
-  const addQuoteHandler = (quoteData) => {
-    console.log(quoteData);
-  };
+const DUMMY_QUOTES = [
+  { id: 'q1', author: 'Max', text: 'Learning React is fun!' },
+  { id: 'q2', author: 'Maximilian', text: 'Learning React is great!' },
+];
 
-  return <QuoteForm onAddQuote={addQuoteHandler} />;
+const AllQuotes = () => {
+  return <QuoteList quotes={DUMMY_QUOTES} />
 };
 
-export default NewQuote;
+export default AllQuotes;
